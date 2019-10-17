ebf9a41b221edc3bbb858c8b9c73ae6a6b99e41a
CAbagnale <50644090+CAbagnale@users.noreply.github.com>
2019-10-17 16:00:33 -0500

    Create List.js

---
+++ List.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Card from './Card';
+import './List.css';
+
+export default function List(props) {
+    return (
+        <section className="List">
+            <header className="List-header">
+                <h2>{props.header}</h2>
+            </header>
+            <div className="List-cards">
+                {props.cards.map((card) => 
+                    <Card 
+                        key={card.id}
+                        id={card.id}
+                        title={card.title}
+                        content={card.content}
+                        onDeleteItem={props.onDeleteItem}
+                    />
+                )}
+                <button 
+                    type="button" 
+                    className="List-add-button"
+                    onClick={() => props.onAddItem(props.id)}
+                >
+                    + Add Random Card
+                </button>
+            </div>
+        </section>
+    )
+}
+
+List.defaultProps = {
+    onClickAdd: () => {},
+}
