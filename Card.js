d9bdfef90ebca43c3b73a33b4a90bd0030e4b52a
CAbagnale <50644090+CAbagnale@users.noreply.github.com>
2019-10-17 16:00:20 -0500

    Create Card.js

---
+++ Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./Card.css";
+
+
+
+export default function Card(props) {
+    return (
+        <div className="Card">
+            <button 
+                type="button"
+                onClick={() => props.onDeleteItem(props.id)}
+            >
+                delete
+            </button>
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    )
+}
+
+Card.propTypes = {
+    onClickDelete: () => {}
+}
